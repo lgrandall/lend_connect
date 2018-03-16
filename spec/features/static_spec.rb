d79ffea5be7f28952a673e798fd1f6353d85b4e8
Louis Randall <louisrandall@Louiss-MacBook-Pro.local>
2018-03-15 19:53:45 -0700

    set up a homepage

---
+++ spec/features/static_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper' 
+
+describe 'navigation' do 
+	describe 'homepage' do 
+		it 'can be reached' do 
+			visit root_path 
+			expect(page.status_code).to eq(200)
+		end
+	end 
+end
